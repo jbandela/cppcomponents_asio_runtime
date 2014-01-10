6480af3379422e4ad9641ff114f9ae86a2c3e005
jbandela <jbandela@gmail.com>
2014-01-09 19:52:21 -0500

    Added build_unit test for linux

---
+++ testing/test/build_test.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+
+g++ -std=c++11 ../unit_test.cpp -I ../../../cppcomponents -I ../../../cppcomponents_async_coroutine_wrapper/ -I ../external/googletest-read-only/ -I ../external/googletest-read-only/include/ ../external/googletest-read-only/src/gtest_main.cc ../external/googletest-read-only/src/gtest-all.cc -pthread -ldl -o unit_test
